cf37f2ff01a2b9681dd5c3f5424fb44628afa223
Ryan Jones <RyanJonesHTX@pm.me>
2023-03-07 23:42:37 -0600

    added forms

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -14,10 +14,15 @@ export class TaskItemComponent {
     reminder: false,
   };
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
 
   faTimes = faTimes;
 
   onDelete(task: Task) {
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 }
