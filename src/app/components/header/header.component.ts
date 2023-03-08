3109f754d02e2f2d8e8346559b23a0627659289f
Ryan Jones <RyanJonesHTX@pm.me>
2023-03-07 23:55:09 -0600

    added toggle on form/button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription;
 
-  toggleAddTask() {
-    console.log('toggle');
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
   }
 
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
 }
